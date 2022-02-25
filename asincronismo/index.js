94e2503596c27be97af14e2c8c8c93d5303dee03
Facundo Abdo <facundo.abdo@fluxit.com.ar>
2022-02-25 13:33:55 -0300

    Ajustes para cumplir la consigna

--- asincronismo/index.js
+++ asincronismo/index.js
@@ -3,7 +3,6 @@
   const lorem2 =
     "Irure consequat in voluptate eu elit consequat non cillum qui dolor nulla qui occaecat.";
   const lorem3 = "Amet anim velit Lorem sit eu sint consectetur amet.";
-  const lorem4 = "Elit ipsum pariatur velit laborum nostrud.";
 
   const recorrerTexto = async (texto, callback, tiempo = 1000) => {
     const palabras = texto.split(" ");
@@ -25,7 +24,10 @@
         if (i === palabras.length) {
           clearInterval(interval); //finalizamos el setInterval
           callback(); //ejecutamos el callback
-          resolve(); //finalizamos la ejecución de recorrerTexto resolviendo el promise
+          resolve(palabras.length);
+          //finalizamos la ejecución de recorrerTexto
+          //resolviendo el promise y devolviendo la cantidad de palabras para
+          //poder acumularlas y contar la cantidad de las 3 pasadas.
         }
       };
 
@@ -41,50 +43,59 @@
   */
 
   // 1) Usando una IIFE global async (ver línea 1)
+  //    NOTA: De ésta manera no es posible contar la cantidad de palabras
+  //    totales de todas las pasadas sin usar variables globales
 
-  await recorrerTexto(lorem1, () => {
-    console.log("Recorrido del texto 1 finalizado");
-    console.log("================================");
+  // await recorrerTexto(lorem1, (cantidadPalabras) => {
+  //   console.log("Recorrido del texto 1 finalizado");
+  //   console.log("================================");
 
-    //Sin especificar valor para el parámetro "tiempo" de modo
-    //que se use el valor por defecto
-  });
-  await recorrerTexto(
-    lorem2,
-    () => {
-      console.log("Recorrido del texto 2 finalizado");
-      console.log("================================");
-    },
-    500
-  );
-  await recorrerTexto(
-    lorem3,
-    () => {
-      console.log("Recorrido del texto 3 finalizado");
-      console.log("================================");
-    },
-    250
-  );
-  await recorrerTexto(
-    lorem4,
-    () => {
-      console.log("Recorrido del texto 4 finalizado");
-      console.log("================================");
-    },
-    100
-  );
+  //   //Sin especificar valor para el parámetro "tiempo" de modo
+  //   //que se use el valor por defecto
+  // });
+  // await recorrerTexto(
+  //   lorem2,
+  //   (cantidadPalabras) => {
+  //     console.log("Recorrido del texto 2 finalizado");
+  //     console.log("================================");
+  //   },
+  //   500
+  // );
+  // await recorrerTexto(
+  //   lorem3,
+  //   (cantidadPalabras) => {
+  //     console.log("Recorrido del texto 3 finalizado");
+  //     console.log("================================");
+  //   },
+  //   250
+  // );
 
   //2) Encadenando la resolución de los promises usando .then()
 
-  // recorrerTexto(lorem1, 1000, () => {
-  //   console.log("Recorrido del texto 1 finalizado");
-  // }).then(() => {
-  //   recorrerTexto(lorem2, 1000, () => {
-  //     console.log("Recorrido del texto 2 finalizado");
-  //   }).then(() => {
-  //     recorrerTexto(lorem3, 1000, () => {
-  //       console.log("Recorrido del texto 3 finalizado");
-  //     });
-  //   });
-  // });
+  recorrerTexto(lorem1, () => {
+    console.log("Recorrido del texto 1 finalizado");
+  }).then((cantidadPalabras) => {
+    let cantidadTotal = cantidadPalabras;
+    recorrerTexto(
+      lorem2,
+      () => {
+        console.log("Recorrido del texto 2 finalizado");
+      },
+      500
+    ).then((cantidadPalabras2) => {
+      cantidadTotal += cantidadPalabras2;
+      recorrerTexto(
+        lorem3,
+        () => {
+          console.log("Recorrido del texto 3 finalizado");
+        },
+        250
+      ).then((cantidadPalabras3) => {
+        cantidadTotal += cantidadPalabras3;
+        console.log(
+          `Proceso completo. Cantidad total de palabras: ${cantidadTotal}`
+        );
+      });
+    });
+  });
 })();
